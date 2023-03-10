d1854a8288960c1870c9540282812b6ba9a20456
satabdiglobalyhub <satabdi.gautam@globalyhub.com>
2023-03-10 11:54:43 +0545

    feat:initialised tailwind

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <CovidHome/>
+</template>
+
+<script>
+import CovidHome from "./components/CovidHome.vue"
+export default{
+  components:{
+    CovidHome,
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/CovidHome.vue
@@ -0,0 +1,6 @@
+<template>
+   <h1 class="underline"> covid</h1>
+</template>
+
+<style>
+</style>
